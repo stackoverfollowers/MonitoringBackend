8e0e94e4d301714ff283f95fda1337d12f39ebc1
kesha1225 <samedov03@mail.ru>
2023-02-17 14:07:24 +0300

    mapper moment

---
+++ kafka_dump.py
@@ -0,0 +1,54 @@
+import json
+import logging
+import traceback
+
+from aiokafka import AIOKafkaConsumer
+from aiokafka.helpers import create_ssl_context
+from kafka import TopicPartition
+
+from config import KafkaConfig
+from consumer.mapper import ExMapper
+from db.db import write_in_base
+
+
+async def consume_forever():
+    consumer = AIOKafkaConsumer(
+        KafkaConfig.TOPIC,
+        client_id=KafkaConfig.CLIENT_ID,
+        security_protocol=KafkaConfig.SECURITY_PROTOCOL,
+        sasl_mechanism=KafkaConfig.SASL_MECHANISM,
+        sasl_plain_username=KafkaConfig.SASL_PLAIN_USERNAME,
+        sasl_plain_password=KafkaConfig.SASL_PLAIN_PASSWORD,
+        ssl_context=create_ssl_context(cafile=KafkaConfig.CONTEXT_FILE),
+        bootstrap_servers=KafkaConfig.BOOTSTRAP_SERVERS,
+        group_id=KafkaConfig.GROUP_ID,
+        request_timeout_ms=1000,
+        enable_auto_commit=False,
+        auto_offset_reset="earliest"
+    )
+    await consumer.start()
+    tp = TopicPartition(KafkaConfig.TOPIC, 0)
+    print(consumer.partitions_for_topic(KafkaConfig.TOPIC,))
+    await consumer.seek_to_beginning(tp)
+
+    while True:
+        try:
+            print("Cool, we'are connected!")
+            async for msg in consumer:
+                msg_dict = msg.__dict__
+                msg_dict["value"] = json.loads(msg_dict["value"].decode("utf-8"))
+
+                mapper = ExMapper(msg_dict["value"])
+                print(mapper.moment)
+
+                result_id = await write_in_base(data=msg_dict)
+                print(f"{result_id=}")
+        except Exception as e:
+            print(traceback.format_exc())
+            logging.error(traceback.format_exc())
+
+
+import asyncio
+
+asyncio.get_event_loop().run_until_complete(consume_forever())
+
